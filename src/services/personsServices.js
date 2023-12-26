398c952287882660f3f3cc78263cbefeff13153f
Victor <vicktorcori@hotmail.com>
2023-12-26 15:07:23 -0300

    fixed api url

--- src/services/personsServices.js
+++ src/services/personsServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://part3-backend-production.up.railway.app/'
+const baseUrl = 'https://part3-backend-production.up.railway.app/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
