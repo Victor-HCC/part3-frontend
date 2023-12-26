7aeab5d04537cdfe0b5cefdbdab39ea28196e4ab
Victor <vicktorcori@hotmail.com>
2023-12-26 15:04:33 -0300

    add url api

--- src/services/personsServices.js
+++ src/services/personsServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://part3-backend-production.up.railway.app/'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
